ebca10fd87b6ba0f9f0168a823382eee18a41df1
David2261 <bulatnasirov2003@gmail.com>
2023-02-27 18:25:13 +0300

    Add SRP

---
+++ UsefulExp/CleanArc/SRP/srp.h
@@ -0,0 +1,12 @@
+#ifndef SRP_H_
+#define SRP_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculatePay(int, int);
+int reportHours(int, int);
+void save(int*, int, int);
+int regularHours(int);
+
+
+#endif // SRP_H_
